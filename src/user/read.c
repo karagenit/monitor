b6dc059a79eb73f64a4b90cb52f9a9ae97999c52
Caleb Smith <karagenit@outlook.com>
2017-12-20 21:40:39 -0500

    Add Read Header

--- src/user/read.c
+++ src/user/read.c
@@ -1,11 +1,4 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define DATA "Half a league, half a league . . ."
+#include "read.h"
 
 int main(int argc, char *argv[])
 {
---
+++ src/user/read.h
@@ -0,0 +1,15 @@
+#ifndef READ_H
+#define READ_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define DATA "Half a league, half a league . . ."
+
+int main(int argc, char *argv[]);
+
+#endif
