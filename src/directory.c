5e4d8a6d31620c406ded0f25cbbd347e14e9e1f4
Caleb Smith <karagenit@outlook.com>
2017-12-29 22:15:04 -0500

    Add check_directory

--- src/directory.c
+++ src/directory.c
@@ -6,18 +6,19 @@ int init_directory(struct Monitor *monitor)
 
     DIR *directory = opendir(monitor->path);
 
-    if (directory) {
-        for(int i = 0; i < LIST_SIZE; i++) {
-            struct dirent *entry = readdir(directory);
-            if(entry == NULL) {
-                break;
-            }
-            strncpy(monitor->dir_list[i], entry->d_name, DIR_SIZE - 1);
-        }
-        closedir(directory);
-    } else {
-        //directory error
+    if (!directory) {
+        return -1;
     }
+
+    for(int i = 0; i < LIST_SIZE; i++) {
+        struct dirent *entry = readdir(directory);
+        if(entry == NULL) {
+            break;
+        }
+        strncpy(monitor->dir_list[i], entry->d_name, DIR_SIZE - 1);
+    }
+
+    closedir(directory);
 }
 
 int check_directory(struct Monitor *monitor)
@@ -25,21 +26,38 @@ int check_directory(struct Monitor *monitor)
     char found[LIST_SIZE];
     memset(found, 0, LIST_SIZE);
 
-    for (int i = 0; i < LIST_SIZE; i++) {
-        if (monitor->dir_list[i][0]) {
-            fprintf(monitor->stream, monitor->dir_list[i]);
+    DIR *directory = opendir(monitor->path);
+
+    if (!directory) {
+        return -1;
+    }
+
+    while (1) {
+        struct dirent *entry = readdir(directory);
+
+        if (entry == NULL) {
+            break;
+        }
+
+        if (0 /*listing_exists(entry->d_name)*/) {
+            //mark good
+        } else {
+            //add listing
+            //print stream +
+            fprintf(monitor->stream, "+");
+            fprintf(monitor->stream, entry->d_name);
             fprintf(monitor->stream, "\n");
+            //mark good
         }
     }
 
-    fflush(monitor->stream);
+    for (int i = 0; i < LIST_SIZE; i++) {
+        //if not good
+        //print stream -
+        //remove from list
+    }
 
-    //grab each directory
-    //if it's a new one, add it to dir_list (if possible, 1024 max)
-    //and print to stream as +, and mark as "good"
-    //if its an existing one, mark as "good"
-    //after, print each that aren't marked "good" to stream as -,
-    //and drop them from the list
+    fflush(monitor->stream);
 
     //weird bug: if there are 1025 items in dir, the last will constantly
     //be seen as a "new" dir (as it can't be stored in the list) - easiest fix
